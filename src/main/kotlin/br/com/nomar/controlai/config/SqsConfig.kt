5f7e7c3ed06f4c9f50250155a4785f594fde8848
Ramon Mesquita <ramonmesquita113@gmail.com>
2026-02-20 21:06:09 -0300

    adiciona config localstack e mysql local

--- src/main/kotlin/br/com/nomar/controlai/config/SqsConfig.kt
+++ src/main/kotlin/br/com/nomar/controlai/config/SqsConfig.kt
@@ -3,6 +3,8 @@ package br.com.nomar.controlai.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.sqs.SqsClient
 import java.net.URI
@@ -10,13 +12,20 @@ import java.net.URI
 @Configuration
 class SqsConfig(
     @Value("\${aws.region}") private val region: String,
-    @Value("\${aws.sqs.endpoint:}") private val endpoint: String
+    @Value("\${aws.sqs.endpoint:}") private val endpoint: String,
+    @Value("\${aws.access-key-id:test}") private val accessKeyId: String,
+    @Value("\${aws.secret-access-key:test}") private val secretAccessKey: String
 ) {
 
     @Bean
     fun sqsClient(): SqsClient {
         val builder = SqsClient.builder()
             .region(Region.of(region))
+            .credentialsProvider(
+                StaticCredentialsProvider.create(
+                    AwsBasicCredentials.create(accessKeyId, secretAccessKey)
+                )
+            )
 
         if (endpoint.isNotBlank()) {
             builder.endpointOverride(URI.create(endpoint))
